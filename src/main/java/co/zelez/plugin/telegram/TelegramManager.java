5e33bf22bab98dfb6df86f6508f3e629ab0b15c7
ZeleZ <zelez-t@outlook.com>
2024-08-29 17:11:00 -0500

    feat: data storage file

--- src/main/java/co/zelez/plugin/telegram/TelegramManager.java
+++ src/main/java/co/zelez/plugin/telegram/TelegramManager.java
@@ -1,11 +1,16 @@
 package co.zelez.plugin.telegram;
 
 import co.zelez.core.command.reader.entity.Param;
-import co.zelez.core.command.reader.usecase.HelpReader;
 import co.zelez.core.command.reader.usecase.ReaderService;
+import co.zelez.core.command.reader.usecase.ShopReader;
+import co.zelez.core.shopping.repository.ShopRepository;
+import co.zelez.core.shopping.usecase.ShoppingService;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import lombok.Generated;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -13,18 +18,33 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 
 public class TelegramManager implements LongPollingSingleThreadUpdateConsumer {
     private final TelegramClient telegramClient;
-    private final HashMap<Long, ReaderService> readerServices = new HashMap<>();
+    private final HashMap<Long, ShopRepository> repository = new HashMap<>();
 
     public TelegramManager(String botToken, TelegramClient client) {
         telegramClient = client;
-        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
-            botsApplication.registerBot(botToken, this);
-            System.out.println("Successfully started!");
-            Thread.currentThread().join();
+
+        try {
+            Gson gson = new Gson();
+            Type type = new TypeToken<HashMap<Long, ShopRepository>>() { }.getType();
+            JsonReader jsonReader = new JsonReader(new FileReader("storage/data.json"));
+            HashMap<Long, ShopRepository> jsonMap = gson.fromJson(jsonReader, type);
+
+            if (jsonMap != null) {
+                repository.putAll(jsonMap);
+            }
+
+            try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+                botsApplication.registerBot(botToken, this);
+                System.out.println("Successfully started!");
+                Thread.currentThread().join();
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -39,10 +59,10 @@ public class TelegramManager implements LongPollingSingleThreadUpdateConsumer {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-            ReaderService readerService;
+            ReaderService reader;
 
-            if (!readerServices.containsKey(chat_id)) {
-                readerServices.put(chat_id, new ReaderService());
+            if (!repository.containsKey(chat_id)) {
+                repository.put(chat_id, new ShopRepository());
                 SendMessage message = SendMessage
                         .builder()
                         .chatId(chat_id)
@@ -54,9 +74,9 @@ public class TelegramManager implements LongPollingSingleThreadUpdateConsumer {
                     throw new RuntimeException(e);
                 }
             }
-            readerService = readerServices.get(chat_id);
-            Param param = readerService.buildParam(message_text);
-            String response = readerService.readParam(param);
+            reader = readerService(repository.get(chat_id));
+            Param param = reader.buildParam(message_text);
+            String response = reader.readParam(param);
 
             SendMessage message = SendMessage
                     .builder()
@@ -66,6 +86,16 @@ public class TelegramManager implements LongPollingSingleThreadUpdateConsumer {
             try {
                 telegramClient.execute(message);
             } catch (TelegramApiException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Gson gson = new Gson();
+
+                String json = gson.toJson(repository);
+                FileWriter file = new FileWriter("storage/data.json");
+                file.write(json);
+                file.close();
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
@@ -79,5 +109,12 @@ public class TelegramManager implements LongPollingSingleThreadUpdateConsumer {
                 Hi %s \uD83D\uDC4B
                 I'm Shopping bot with the mission of help your daily purchases \uD83D\uDED2
                 """, userName);
+    }
+
+    @Generated
+    private ReaderService readerService(ShopRepository repository) {
+        ShoppingService service = new ShoppingService(repository);
+        ShopReader reader = new ShopReader(service);
+        return new ReaderService(reader);
     }
 }
